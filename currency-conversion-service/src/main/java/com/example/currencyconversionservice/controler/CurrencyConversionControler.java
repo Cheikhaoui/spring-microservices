39d7df201d4787f3ab94099caddbc58361ec1a16
cheik <cheikhaoui.hicham@gmail.com>
2023-05-29 13:01:22 +0100

    add configurations for k8s + delete dependencies of tracing service + eureka

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/controler/CurrencyConversionControler.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/controler/CurrencyConversionControler.java
@@ -2,6 +2,7 @@ package com.example.currencyconversionservice.controler;
 
 import com.example.currencyconversionservice.model.CurrencyConversion;
 import com.example.currencyconversionservice.service.CurencyExchangeProxy;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,24 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
+@Log4j2
 @RestController
-public class CurrencyExchangeControler {
+public class CurrencyConversionControler {
 
     private final CurencyExchangeProxy curencyExchangeProxy;
 
-    public CurrencyExchangeControler(CurencyExchangeProxy curencyExchangeProxy) {
+    public CurrencyConversionControler(CurencyExchangeProxy curencyExchangeProxy) {
         this.curencyExchangeProxy = curencyExchangeProxy;
     }
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity")
     CurrencyConversion getCurrencyConversion(@PathVariable String from , @PathVariable String to ,
                                              @RequestParam("quantity")BigDecimal quantity){
+        log.info("calculate currency conversion called with {} to {} whith {}",from,to,quantity);
         CurrencyConversion currencyConversion = curencyExchangeProxy.getExchangeValue(from,to);
         return CurrencyConversion.builder().from(currencyConversion.getFrom()).to(currencyConversion.getTo())
                 .environment(currencyConversion.getEnvironment()).conversionMultiple(currencyConversion.getConversionMultiple())
                 .id(currencyConversion.getId()).quantity(quantity).totalCalculatedAmount(quantity.multiply(currencyConversion.getConversionMultiple()))
                 .build();
     }
-
 
 }
--- exchange-service/src/main/java/com/example/exchangeservice/controler/CurrencyExchangeControler.java
+++ exchange-service/src/main/java/com/example/exchangeservice/controler/CurrencyExchangeControler.java
@@ -2,11 +2,13 @@ package com.example.exchangeservice.controler;
 
 import com.example.exchangeservice.model.CurrencyExchange;
 import com.example.exchangeservice.repositories.CurrencyExchangeRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 public class CurrencyExchangeControler {
     private final CurrencyExchangeRepository currencyExchangeRepository;
@@ -20,7 +22,10 @@ public class CurrencyExchangeControler {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyExchange getCurrencyExchange(@PathVariable String from, @PathVariable String to) {
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
-        currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
+        String port = environment.getProperty("local.server.port");
+        String host = environment.getProperty("HOSTNAME");
+        String version ="v11";
+        currencyExchange.setEnvironment(port+" "+version + " "+host);
         return currencyExchange;
     }
 }
